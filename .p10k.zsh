1eae343a80eee6e21a3a749dab1d59ecbc382fc0
Carl Wilhelm Linder Rosén <cwlr@sudosweden.com>
2022-10-24 09:11:40 +0000

    Show different colors for 'prod' and 'dev' contexts

--- .p10k.zsh
+++ .p10k.zsh
@@ -1207,9 +1207,16 @@
   typeset -g POWERLEVEL9K_KUBECONTEXT_CLASSES=(
       # '*prod*'  PROD    # These values are examples that are unlikely
       # '*test*'  TEST    # to match your needs. Customize them as needed.
+      '*prod*'  PROD
+      '*dev*'  DEV
       '*'       DEFAULT)
   typeset -g POWERLEVEL9K_KUBECONTEXT_DEFAULT_FOREGROUND=5
   # typeset -g POWERLEVEL9K_KUBECONTEXT_DEFAULT_VISUAL_IDENTIFIER_EXPANSION='⭐'
+
+  typeset -g POWERLEVEL9K_KUBECONTEXT_PROD_FOREGROUND=5
+  typeset -g POWERLEVEL9K_KUBECONTEXT_DEV_FOREGROUND=3
+
+
 
   # Use POWERLEVEL9K_KUBECONTEXT_CONTENT_EXPANSION to specify the content displayed by kubecontext
   # segment. Parameter expansions are very flexible and fast, too. See reference:
